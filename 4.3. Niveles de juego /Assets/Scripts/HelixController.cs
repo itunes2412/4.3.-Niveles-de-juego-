3e33d7c264f2a4564b4aab2df1b6c5adaa3a977e
itunes2412 <itunes2412@gmail.com>
2023-04-29 17:25:59 -0600

    initial commit

---
+++ 4.3. Niveles de juego /Assets/Scripts/HelixController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixController : MonoBehaviour
+{
+
+    private Vector2 lastTapPosition;
+
+    private Vector3 startPosition;
+
+    private Transform topTransform;
+    private Transform goalTransform;
+
+    public GameObject helixLevelPrefab;
+
+    public List<Stage> allStages = new List<Stage>();
+
+    public float helixDistance;
+
+    private List<GameObject> spawnedLevels = new List<GameObject>();
+
+    
+    //Continuacion act 4.4 Video 7
+    //private void Awake()
+    //{
+       // startPosition = transform.localEulerAngles;
+       // helixDistance = topTransform.localPosition.y - (goalTransform.localPosition.y + .1f);
+        //LoadStage(0);
+   // }
+
+    void Start()
+    {
+        startPosition = transform.localEulerAngles;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector2 currentTapPosition = Input.mousePosition;
+
+            if(lastTapPosition==Vector2.zero)
+            {
+                lastTapPosition = currentTapPosition;
+            }
+
+            float distance = lastTapPosition.x - currentTapPosition.x;
+            lastTapPosition = currentTapPosition;
+
+            transform.Rotate(Vector3.up * distance);
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            lastTapPosition = Vector2.zero;
+        }
+    }
+
+    public void LoadStage(int stageNumber)
+    {
+
+    }
+}
